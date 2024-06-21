6e626f521d94dc8fa1658c55dbd399db892dac84
Necromunger <lil_tommy_tom@hotmail.com>
2024-06-22 02:21:46 +1000

    Cleaned up item placement, check for flat surface, keep rotation even

--- Assets/Scripts/SS3D/Systems/Inventory/Interactions/DropInteraction.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Interactions/DropInteraction.cs
@@ -11,6 +11,11 @@ namespace SS3D.Systems.Inventory.Interactions
     [Serializable]
     public class DropInteraction : Interaction
     {
+        /// <summary>
+        /// The maximum angle of surface the item will allow being dropped on
+        /// </summary>
+        private float _maxSurfaceAngle = 10;
+
         public override string GetName(InteractionEvent interactionEvent)
         {
             return "Drop";
@@ -35,11 +40,21 @@ namespace SS3D.Systems.Inventory.Interactions
 
         public override bool Start(InteractionEvent interactionEvent, InteractionReference reference)
         {
+            // we consider if the surface we are placing the item on is flat
+            float angle = Vector3.Angle(interactionEvent.Normal, Vector3.up);  
+            if (angle > _maxSurfaceAngle)
+            {
+                return false;
+            }
+
             // we check if the source of the interaction is a hand
             if (interactionEvent.Source.GetRootSource() is Hand hand)
             {
+                // we rotate the item based on the facing direction of the hand
+                Quaternion rotation = Quaternion.Euler(0, hand.ItemInHand.transform.eulerAngles.y, 0);
+
                 // we place the item in the hand in the point we clicked
-                hand?.PlaceHeldItemOutOfHand(interactionEvent.Point, hand.ItemInHand.transform.rotation);
+                hand?.PlaceHeldItemOutOfHand(interactionEvent.Point, rotation);
             }
 
             return false;
